96d886ccdcd64f56f3b93f2a045c13a3e2dc9f21
Amogh <amoghak2004@gmail.com>
2025-05-01 13:59:41 +0530

    added siri wave with opening sound of assistant

---
+++ engine/features.py
@@ -0,0 +1,6 @@
+from playsound import playsound
+
+# playing sound of jarvis
+def playAssistantSound():
+    music_dir = r"C:\Users\amogh\jarvis\www\assets\audio\www_assets_audio_start_sound.mp3"
+    playsound(music_dir, True)
--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import os
-
 import eel
+from engine.features import playAssistantSound
 
 eel.init("www")
 
+playAssistantSound()  # <- Function call corrected
+
+# Open in app mode (Microsoft Edge)
 os.system('start msedge.exe --app="http://localhost:8000/index.html"')
 
 eel.start("index.html", mode=None, host="localhost", block=True)
